629a35ec96ddca9639ee450c3d0fec1df741af9b
Nuku Ameyibor <nayibor@gmail.com>
2021-08-24 18:21:15 +0000

    xref and dialyzer checks added
    xref and dialyzer checks added to code
    command to be executed as well as number of times to
    be executed added as parameter to function check_multiple/2

--- src/erlchroma_port.erl
+++ src/erlchroma_port.erl
@@ -2,14 +2,18 @@
 
 -export([]).
 
--export([start/0,check_multiple/1,start/1, stop/1, init/1]).
+-export([start/0,check_multiple/2,start/1, stop/1, init/1]).
 
 
-check_multiple(N)->
-	Command = "fpcalc -ts -chunk 2 -overlap -json http://yfm1079accra.atunwadigital.streamguys1.com/yfm1079accra",
+%% @doc for testing for running multiple instances of a command 
+%%Command = "fpcalc -ts -chunk 2 -overlap -json http://yfm1079accra.atunwadigital.streamguys1.com/yfm1079accra",
+-spec check_multiple(integer(),binary()|string())->list().
+check_multiple(N,Command)->
 	List_pids = lists:map(fun(_) -> start(Command) end,lists:seq(1,N)).
 
 
+%% @doc for starting the port command
+-spec start()->error_no_prog_specified | pid().
 start() ->
     error_no_prog_specified.
 
@@ -18,17 +22,22 @@ start(ExtPrg) ->
     spawn(?MODULE, init, [ExtPrg]).
 
 
-
+%% @doc for stopping the port program
+-spec stop(pid())-> stop.
 stop(Pid) ->
     Pid ! stop.
 
 
+%%for initilizing the port for runnig commands
+-spec init([string() | char()]) -> pid().
 init(ExtPrg) ->
     process_flag(trap_exit, true),
     {ok,Pid,Ospid} = exec:run(ExtPrg, [stdout, stderr,monitor]),
     loop(Pid,Ospid,ExtPrg).
 
 
+%%for looping and execting main function
+-spec loop(pid(),integer(),[string() | char()]) -> pid().
 loop(Pid,Ospid,ExtPrg) ->
     receive
 		{'DOWN',Ospid,process,Pid,normal}->
